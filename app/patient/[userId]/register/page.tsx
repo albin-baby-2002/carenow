c0e34dfb68df1fb0ca3be6ddfc32f995d1fd7831
Albin Baby <albinbtg@gmail.com>
2024-07-24 09:48:53 +0530

    created the register page and its basic design

---
+++ app/patient/[userId]/register/page.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Register = () => {
+  return (
+    <div className="flex h-screen max-h-screen">
+      {/* Todo OTP verification */}
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
+
+          {/* <PatientForm /> */}
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="text-dark-600 xl:text-left">© 2024 CareNow</p>
+            <Link href={"/?admin=true"} className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <Image
+        src={"/assets/images/register-img.png"}
+        height={1000}
+        width={1000}
+        alt="register_hero_img"
+        className="side-img max-w-[390px]"
+      />
+    </div>
+  );
+};
+
+export default Register;
