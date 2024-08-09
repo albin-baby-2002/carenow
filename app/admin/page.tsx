0714fe7c5d9d0e1234f118077dc4a422badfd78d
Albin Baby <albinbtg@gmail.com>
2024-08-09 09:09:58 +0530

    created the basic design of the admin page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,11 +1,77 @@
-import React from 'react'
+import {DataTable} from "@/components/DataTable";
+import StatCard from "@/components/StatCard";
+import {columns, Payment} from "@/components/table/columns";
+import { getRecentAppointments } from "@/lib/actions/appointment.action";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
-const Admin = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    // ...
+  ];
 }
 
-export default Admin
+const Admin = async() => {
+  const appointments = await getRecentAppointments();
+  const data = await getData()
+  
+  return (
+    <div className="mx-auto flex max-w-[1500px] flex-col">
+      <header className="admin-header">
+        <Link href={"/"} className="cursor-pointer">
+          <Image
+            src={"/assets/icons/logo-full.svg"}
+            height={32}
+            width={162}
+            alt="logo"
+            className="h-8 w-fit"
+          />
+        </Link>
+
+        <p className="text-16-semibold">Admin Dashboard</p>
+      </header>
+
+      <main className="admin-main">
+        <section className="w-full space-y-4">
+          <h1>Welcome 👋</h1>
+          <p className="text-dark-700">
+            Start the day with managing new appointments
+          </p>
+        </section>
+
+        <section className="admin-stat">
+          <StatCard
+            type="appointments"
+            count={appointments.scheduledCount}
+            label={"scheduled appointments"}
+            icon={"/assets/icons/appointments.svg"}
+          />
+          <StatCard
+            type="pending"
+            count={appointments.pendingCount}
+            label={"Pending appointments"}
+            icon={"/assets/icons/pending.svg"}
+          />
+          <StatCard
+            type="cancelled"
+            count={appointments.cancelledCount}
+            label={"Cancelled appointments"}
+            icon={"/assets/icons/cancelled.svg"}
+          />
+        </section>
+        
+        <DataTable columns={columns} data={data}/>
+      </main>
+    </div>
+  );
+};
+
+export default Admin;
--- lib/actions/appointment.action.ts
+++ lib/actions/appointment.action.ts
@@ -1,9 +1,12 @@
-"use server"
-import { ID } from "node-appwrite";
+"use server";
+import { ID, Query } from "node-appwrite";
 import { databases } from "../appwrite.config";
 import { parseStringify } from "../utils";
+import { Appointment } from "@/types/appwrite.type";
 
-export const createAppointment = async (appointmentData:CreateAppointmentParams) => {
+export const createAppointment = async (
+  appointmentData: CreateAppointmentParams,
+) => {
   try {
     try {
       let appointment = await databases.createDocument(
@@ -20,18 +23,58 @@ export const createAppointment = async (appointmentData:CreateAppointmentParams)
   } catch (error) {}
 };
 
-export const getAppointment = async (appointmentId:string)=>{
+export const getAppointment = async (appointmentId: string) => {
   try {
-    
     const appointment = await databases.getDocument(
       process.env.DATABASE_ID!,
       process.env.APPOINTMENTS_COLLECTION_ID!,
-      appointmentId
+      appointmentId,
+    );
+
+    return appointment;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getRecentAppointments = async () => {
+  try {
+    const appointments = await databases.listDocuments(
+      process.env.DATABASE_ID!,
+      process.env.APPOINTMENTS_COLLECTION_ID!,
+      [Query.orderDesc("$createdAt")],
+    );
+
+    const intialCounts = {
+      scheduledCount: 0,
+      pendingCount: 0,
+      cancelledCount: 0,
+    };
+
+    const counts = (appointments.documents as Appointment[]).reduce(
+      (acc, appointments) => {
+        if (appointments.status === "scheduled") {
+          acc.scheduledCount += 1;
+        } else if (appointments.status === "pending") {
+          acc.pendingCount += 1;
+        } else if (appointments.status === "cancelled") {
+          acc.cancelledCount += 1;
+        }
+
+        return acc;
+      },
+      intialCounts,
     );
     
-    return appointment
+    const data = {
+      total:appointments.total,
+      ...counts,
+      documents:appointments.documents
+    }
+    
+    return parseStringify(data)
   } catch (error) {
     
     console.log(error)
   }
-}
+};
